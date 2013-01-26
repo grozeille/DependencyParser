8e824bed380a223765978290c07b0690c2ce3982
Alexandre Victoor <alexvictoor@gmail.com>
2013-01-26 22:57:11 +0100

    First LCOM4 analyzer implementation

---
+++ DependencyParser.Test/Lcom4AnalyzerTest.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace DependencyParser.Test {
+
+
+	[NUnit.Framework.TestFixture]
+	public class Lcom4AnalyzerTest {
+		[Test]
+		public void Should_Find_One_Block_On_A_Simple_Class()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.SimpleClass"));
+			Assert.AreEqual(1, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Find_One_Block_On_A_Class_With_Strong_Cohesion()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.ClassWithManyMethodsAndStrongCohesion"));
+			Assert.AreEqual(1, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Treat_Accessors_As_Normal_Methods()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.ClassWithProperties"));
+			Assert.AreEqual(2, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Ignore_Constructors()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.SimpleClassWithCtr"));
+			Assert.AreEqual(2, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Not_Fail__On_Empty_Interface()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.IEmpty"));
+			Assert.AreEqual(0, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Not_Fail__On_Empty_Class()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.EmptyClass"));
+			Assert.AreEqual(0, blocks.Count);
+		}
+
+		[Test]
+		public void Should_Not_Take_In_Account_Dispose_Methods()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.SimpleDisposableClass"));
+			Assert.AreEqual(1, blocks.Count);
+		}
+		
+		[Test]
+		public void Should_Not_Take_In_Account_Equals_Methods()
+		{
+			var analyzer = new Lcom4Analyzer();
+			var blocks = analyzer.FindLcomBlocks(getType("DependencyParser.Test.SimpleClassWithEquals"));
+			Assert.AreEqual(2, blocks.Count);
+		}
+
+		private TypeDefinition getType(string name)
+		{
+			string unit = Assembly.GetExecutingAssembly().Location;
+			var assembly = AssemblyDefinition.ReadAssembly(unit);
+			return assembly.MainModule.GetType(name);
+		}
+
+	}
+
+	public class EmptyClass
+	{
+		
+	}
+
+	public interface IEmpty {
+
+	}
+
+	public class SimpleClass
+	{
+		private int counter;
+
+		public void Increment()
+		{
+			counter++;
+		}
+
+		public void Decrement()
+		{
+			counter--;
+		}
+	}
+
+	public class SimpleClassWithCtr {
+		private int fieldA;
+
+		private int fieldB;
+
+		public SimpleClassWithCtr()
+		{
+			fieldA = 42;
+			fieldB = 36;
+		}
+
+		public void doA()
+		{
+			fieldA++;
+		}
+
+		public void doB()
+		{
+			fieldB++;
+		}
+	}
+
+	public class ClassWithManyMethodsAndStrongCohesion {
+		
+		public void doA()
+		{
+			Foo();
+		}
+
+		public void doB()
+		{
+			Foo();
+			Bar();
+		}
+
+
+		public void doC()
+		{
+			Bar();
+		}
+
+		private static void Foo()
+		{
+			Console.WriteLine("Whatever");
+		}
+
+		private static void Bar()
+		{
+			Console.WriteLine("Whatever");
+		}
+	}
+
+	public class ClassWithProperties {
+		public int Counter { get; set; }
+
+		public string Name { get; set; }
+	}
+
+	public class SimpleDisposableClass : IDisposable
+	{
+		public void doSomething()
+		{
+			Console.WriteLine("Whatever");
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class SimpleClassWithEquals {
+		private int fieldA;
+
+		private int fieldB;
+
+		public void doA()
+		{
+			fieldA++;
+		}
+
+		public void doB()
+		{
+			fieldB++;
+		}
+
+		public bool Equals(SimpleClassWithEquals other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return other.fieldA == fieldA && other.fieldB == fieldB;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof (SimpleClassWithEquals)) return false;
+			return Equals((SimpleClassWithEquals) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (fieldA*397) ^ fieldB;
+			}
+		}
+
+		public static bool operator ==(SimpleClassWithEquals left, SimpleClassWithEquals right)
+		{
+			return Equals(left, right);
+		}
+
+		public static bool operator !=(SimpleClassWithEquals left, SimpleClassWithEquals right)
+		{
+			return !Equals(left, right);
+		}
+	}
+}
